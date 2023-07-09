2f0c2f20c3b344dd04ac667f7ed7f03185419fb9
unknown <maria137.art@hotmail.co.uk>
2023-07-09 17:15:03 +0100

    Cleaning comments

--- mr_select_inbetween_nurb_controls.py
+++ mr_select_inbetween_nurb_controls.py
@@ -1,7 +1,7 @@
 """
 # ------------------------------------------------------------------------------ #
 # SCRIPT: mr_select_inbetween_nurb_controls.py
-# VERSION: 0002
+# VERSION: 0003
 #
 # CREATORS: Maria Robertson
 # ---------------------------------------
@@ -23,6 +23,7 @@
 # ---------------------------------------
 # RUN COMMAND:
 # ---------------------------------------
+e.g.
 
 import importlib
 import mr_select_inbetween_nurb_controls
@@ -41,10 +42,14 @@ mr_select_inbetween_nurb_controls.mr_select_inbetween_nurb_controls()
 # CHANGELOG:
 # ---------------------------------------
 # 2023-07-09 - 0002:
-#   - Finished second pass of script, to work in both top-to-bottom and bottom-to-top hierarchies
+#   - Cleaning comments.
+#   - Removing unneeded check for constraints.
+#
+# 2023-07-09 - 0002:
+#   - Finished second pass of script, to work in both top-to-bottom and bottom-to-top hierarchies.
 #
 # 2023-07-03 - 0001: 
-#   - First pass
+#   - First pass.
 # ------------------------------------------------------------------------------ #
 """
 
@@ -52,82 +57,79 @@ import maya.cmds as cmds
 
 def mr_select_inbetween_nurb_controls():
 
-    ##################################################################
-    # Check if only two objects are selected
+    # ------------------------------------------------------------------- 
+    # 01. CHECK IF ONLY TWO OBJECTS ARE SELECTED
+    # ------------------------------------------------------------------- 
     sel = cmds.ls(selection=True)
 
     if len(sel) != 2:
         cmds.warning("Please select exactly two objects.")
         return
-    ##################################################################
-    # Check if selected controls are NURB curves
 
-    # For every selected object
+    # ------------------------------------------------------------------- 
+    # 01. CHECK IF SELECTED CONTROLS ARE NURB CURVE TRANSFORMS
+    # ------------------------------------------------------------------- 
     for obj in sel:
-        # Check if it is a transform node of a NURB curve
         shape = cmds.listRelatives(obj, shapes=True)
         if shape and cmds.nodeType(shape[0]) == "nurbsCurve":
             continue
         else:
             cmds.warning("Both selected objects must be NURB Curve transforms.")
             return
-    ##################################################################
 
-    # Check if objects are in same hierarchy
+    # ------------------------------------------------------------------- 
+    # 01. CHECK IF OBJECTS SHARE SAME HIERARCHY
+    # -------------------------------------------------------------------
+    # Declare and initialize variables.
     first_item = sel[0]
     second_item = sel[1]
 
-    if len(sel) != 2:
-        cmds.warning("Please select exactly two objects.")
-        return
-            
     first_fullpath = cmds.ls(first_item, long=True)[0]
     second_fullpath = cmds.ls(second_item, long=True)[0]
 
-    first_object_names = first_fullpath.split('|')       
-    second_object_names = second_fullpath.split('|')       
+    first_object_hierarchy = first_fullpath.split('|')       
+    second_object_hierarchy = second_fullpath.split('|')       
 
-    # If objects are in same hierarchy, append them to variable
-    found_objects = []
-    transforms_nodes = []    
-    mode = []
-    if first_item in second_object_names:
-        mode = "top-to-bottom"
-        for obj in second_object_names[1:]:
-            found_objects.append(obj)
+    hierarchy_objects = []
+    order = []
+
+    # If objects are in same hierarchy, add them to hierarchy_objects variable.
+    if first_item in second_object_hierarchy:
+        order = "top-to-bottom"
+        for obj in second_object_hierarchy[1:]:
+            hierarchy_objects.append(obj)
         
-    elif second_item in first_object_names:
-        mode = "bottom-to-top"
-        for obj in first_object_names[1:]:
-            found_objects.append(obj)
+    elif second_item in first_object_hierarchy:
+        order = "bottom-to-top"
+        for obj in first_object_hierarchy[1:]:
+            hierarchy_objects.append(obj)
     else:
         cmds.warning("Must select two objects in same branch")
         return
-    #####################################################
 
-    # Select only stuff inbetween the two items
+    # ------------------------------------------------------------------- 
+    # 01. SELECT ONLY OBJECTS INBETWEEN SELECTION
+    # -------------------------------------------------------------------
+    # Find the index of the selected objects in hierarchy_objects.
+    index1 = hierarchy_objects.index(first_item)
+    index2 = hierarchy_objects.index(second_item)
 
-    # Find the index of the selected objects in the list
-    index1 = found_objects.index(first_item)
-    index2 = found_objects.index(second_item)
+    inbetween_objects = hierarchy_objects[min(index1, index2):max(index1, index2) + 1]
 
-    # Select the objects between the two selected objects
-    inbetween_objects = found_objects[max(index1, index2):min(index1, index2) - 1:-1]  # Reverse the selection order
-    # reverse selection
-    inbetween_objects = inbetween_objects[::-1] 
+    # Filter out unwanted transform types in inbetween_objects.
+    selected_transforms = [obj for obj in inbetween_objects if cmds.nodeType(obj) == "transform"]
 
-    # Filter out constraints from the selection
-    constraint_types = {"pointConstraint", "orientConstraint", "scaleConstraint", "aimConstraint", "parentConstraint"}
-    selected_transforms = [obj for obj in inbetween_objects if cmds.nodeType(obj) == "transform" and not cmds.listRelatives(obj, type=list(constraint_types))]
-
-    # Select only the NURBS curve transforms from the list
+    # Create list of only NURBS curve transforms from selected_transforms.
     nurbs_curve_transforms = [transform for transform in selected_transforms if cmds.listRelatives(transform, shapes=True) and cmds.nodeType(cmds.listRelatives(transform, shapes=True)[0]) == "nurbsCurve"]
 
     if not nurbs_curve_transforms:
         cmds.warning("No NURBS curve transforms found between the selected objects.")
         return
 
-    if mode == "top-to-bottom":
+    # ------------------------------------------------------------------- 
+    # 01. FINISH SCRIPT WITH FOUND NURB CURVE TRANSFORMS SELECTED
+    # -------------------------------------------------------------------
+    if order == "top-to-bottom":
         cmds.select(nurbs_curve_transforms, replace=True)
     else:
         reversed_nurbs_curve_transforms = nurbs_curve_transforms[::-1] 
