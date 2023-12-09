3f03a8c00061d05db82cbaef19b6500ddc3cd444
unknown <maria137.art@hotmail.co.uk>
2023-12-09 02:39:48 +0000

    Made FPS division definable outside of script

--- mr_nextPrevFrame_byFPS.py
+++ mr_nextPrevFrame_byFPS.py
@@ -1,10 +1,7 @@
-import maya.cmds as cmds
-import maya.mel as mel
-
 """
 # ------------------------------------------------------------------------------ #
 # SCRIPT: mr_nextPrevFrame_byFPS.py
-# VERSION: 0001
+# VERSION: 0002
 #
 # CREATORS: Maria Robertson
 # ---------------------------------------
@@ -26,33 +23,48 @@ import maya.mel as mel
 import importlib
 
 import mr_nextPrevFrame_byFPS
-importlib.reload(mr_nextPrevFrame_byFPS)
+importlib.reload(nextFrame)
 
-mr_nextPrevFrame_byFPS.mr_nextFrame_oneSixth_FPS()
+mr_nextPrevFrame_byFPS.nextFrame(7)
+# OR
+mr_nextPrevFrame_byFPS.prevFrame(7)
 
+# ---------------------------------------
+# CHANGELOG:
+# ---------------------------------------
+# 2023-12-08 - 0002:
+# - Made FPS_division definable outside the script.
+#
+# 2023-04-14 - 0001:
+# - First pass.
+#
 # ------------------------------------------------------------------------------ #
 """
 
-divide_FPS_by = 6
+import maya.cmds as cmds
+import maya.mel as mel
 
 
-def mr_nextFrame_oneSixth_FPS():
-    
-    FPS = mel.eval('currentTimeUnitToFPS')
-    frame_jump = (FPS / divide_FPS_by) - 1
+def nextFrame(FPS_division):
+    frame_jump = calculate_frame_jump(FPS_division)
     
     currentTime = cmds.currentTime(q=True)
-    newTime = currentTime + frame_jump
-    
+    newTime = currentTime + frame_jump 
     cmds.currentTime(newTime)
 
 
-def mr_prevFrame_oneSixth_FPS():
-    
-    FPS = mel.eval('currentTimeUnitToFPS')
-    frame_jump = (FPS / divide_FPS_by) - 1
-    
+# ------------------------------------------------------------------------------ #
+def prevFrame(FPS_division):
+    frame_jump = calculate_frame_jump(FPS_division)
+
     currentTime = cmds.currentTime(q=True)
     newTime = currentTime - frame_jump
-    
     cmds.currentTime(newTime)
+
+
+# ------------------------------------------------------------------------------ #
+def calculate_frame_jump(FPS_division):
+    FPS = mel.eval('currentTimeUnitToFPS')
+    frame_jump = (FPS // FPS_division) - 1
+
+    return frame_jump
