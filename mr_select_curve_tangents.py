9285bf91212aef96a4e64451025b452e6eaabc6a
unknown <maria137.art@hotmail.co.uk>
2023-12-17 18:12:59 +0000

    Adding option to toggle between handles

--- mr_select_curve_tangents.py
+++ mr_select_curve_tangents.py
@@ -18,13 +18,18 @@ import importlib
 import mr_select_curve_tangents
 importlib.reload(mr_select_curve_tangents)
 
-# Use one of the following commands:
-mr_select_curve_tangents.main(inTangent=True, outTangent=False)
-mr_select_curve_tangents.main(inTangent=False, outTangent=True)
+# For toggling.
+mr_select_curve_tangents.toggle()
+# For specific selection:
+mr_select_curve_tangents.main(tangent_handle='outTangent')
+mr_select_curve_tangents.main(tangent_handle='inTangent')
 
 # ---------------------------------------
 # CHANGELOG:
 # ---------------------------------------
+# 2023-12-17 - 0002:
+# - Make script cycle between selecting inTangent and outTangent handles.
+#
 # 2023-12-17 - 0001:
 # - First pass.
 # ------------------------------------------------------------------------------ #
@@ -32,18 +37,35 @@ mr_select_curve_tangents.main(inTangent=False, outTangent=True)
 
 import pymel.core as pm
 
-def main(inTangent=None, outTangent=None):
-	anim_curves = pm.keyframe(query=True, name=True)
+def main(tangent_handle=None):
+    if tangent_handle not in ['inTangent', 'outTangent']:
+        pm.warning("Invalid tangent_handle. Please use 'inTangent' or 'outTangent'.")
+        return
 
-	for curve in anim_curves:
-		keys = pm.keyframe(curve, query=True, timeChange=True, selected=True)
-		
-		if inTangent:
-			for key in keys:
-				pm.selectKey(curve, add=True, inTangent=True, time=key)
-				pm.selectKey(curve, remove=True, outTangent=True, time=key)
-						
-		if outTangent:
-			for key in keys:
-				pm.selectKey(curve, remove=True, inTangent=True, time=key)
-				pm.selectKey(curve, add=True, outTangent=True, time=key)
+    anim_curves = pm.keyframe(query=True, name=True)
+
+    for curve in anim_curves:
+        keys = pm.keyframe(curve, query=True, timeChange=True, selected=True)
+
+        for key in keys:
+            if tangent_handle == 'inTangent':
+                pm.selectKey(curve, add=True, inTangent=True, time=key)
+                pm.selectKey(curve, remove=True, outTangent=True, time=key)
+                
+            elif tangent_handle == 'outTangent':
+                pm.selectKey(curve, remove=True, inTangent=True, time=key)
+                pm.selectKey(curve, add=True, outTangent=True, time=key)
+
+# Toggle between tangent handles.
+def toggle():
+    global last_tangent_type
+
+    if 'last_tangent_type' not in globals():
+        last_tangent_type = 'inTangent'
+
+    if last_tangent_type == 'inTangent':
+        main(tangent_handle='outTangent')
+        last_tangent_type = 'outTangent'
+    else:
+        main(tangent_handle='inTangent')
+        last_tangent_type = 'inTangent'
