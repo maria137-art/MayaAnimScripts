0526254041c6a9845191c68b09c574bfccec3f19
unknown <maria137.art@hotmail.co.uk>
2023-12-28 22:30:32 +0000

    Fixed script to scale curves individually.
    Had to convert the MEL version:
    float $keyVals[] = `keyframe -query -selected -valueChange -attribute $curve` ;
    to
    key_vals = cmds.keyframe(curve, query=True, selected=True, valueChange=True)

--- mr_keyframe_scaler.py
+++ mr_keyframe_scaler.py
@@ -1,7 +1,7 @@
 """
 # ------------------------------------------------------------------------------ #
 # SCRIPT: mr_keyframe_scaler.py
-# VERSION: 0001
+# VERSION: 0002
 #
 # CREATORS: Maria Robertson
 # CREDIT: David Peers (for the original keyScaler.mel script) - https://web.archive.org/web/20040816235635/http://andrewsilke.com/mel_info.html
@@ -40,6 +40,9 @@ mr_keyframe_scaler.main(0.8)
 # ---------------------------------------
 # CHANGELOG:
 # ---------------------------------------
+# 2023-12-28 - 0002:
+#   - Stopped script from scaling multiple curves altogether rather than individually.
+#
 # 2023-12-28 - 0001:
 #   - First pass of converting keyScaler.mel script to Python.
 #   - If BaseAnimation is locked, temporarily unlock it, to allow anim_curves on animation layers to be scaled.
@@ -49,6 +52,19 @@ mr_keyframe_scaler.main(0.8)
 import maya.cmds as cmds
 
 def main(scale_amount=None):
+
+    # ------------------------------------------------------------------- 
+    # 01. CHECK SELECTED ANIMATION CURVES
+    # -------------------------------------------------------------------
+
+    sel_curves = cmds.keyframe(query=True, name=True, selected=True)
+    if not sel_curves:
+        cmds.warning("No animation curves selected.")
+        return
+
+    # ------------------------------------------------------------------- 
+    # 01. CHECK IF BASE ANIMATION LAYER IS LOCKED.
+    # -------------------------------------------------------------------
     base_anim_layer = "BaseAnimation"
 
     if cmds.objExists(base_anim_layer):
@@ -56,26 +72,19 @@ def main(scale_amount=None):
         if was_locked:
             was_locked = cmds.animLayer(base_anim_layer, edit=True, lock=False)
 
-    # Make array of selected curves.
-    sel_curves = cmds.keyframe(query=True, name=True, selected=True)
-
-    # Loop scaler for each curve.
+    # ------------------------------------------------------------------- 
+    # 01. SCALE EACH CURVE.
+    # -------------------------------------------------------------------
     for curve in sel_curves:
-        # Array for keys on a single curve.
-        key_vals = cmds.keyframe(query=True, selected=True, valueChange=True, attribute=curve)
-        key_index = cmds.keyframe(query=True, selected=True, indexValue=True, attribute=curve)
+        # Make an array for keys on a single curve.
+        key_vals = cmds.keyframe(curve, query=True, selected=True, valueChange=True)
+        key_index = cmds.keyframe(curve, query=True, selected=True, indexValue=True)
         index_size = len(key_index)
         index_last = key_index[index_size - 1]
 
-        # Get the destination attribute from the list of connections
-        curve_obj = cmds.listConnections(curve + ".output", plugs=True, destination=True)
-        if curve_obj:
-            curve_obj = curve_obj[0]
-        else:
-            continue  # Skip if there are no connections
-
-        # Get the attribute type
-        curve_attr = cmds.listAttr(curve_obj)
+        # Get the destination attribute from the list of connections.
+        curve_obj = cmds.listConnections(curve, plugs=True)
+        curve_attr = cmds.listAttr(curve_obj[0])
 
         max_key = -1000000
         min_key = 1000000
@@ -88,19 +97,20 @@ def main(scale_amount=None):
         mid_point = (max_key + min_key) / 2
         key_insert_start = int(key_index[0])
         key_insert_end = int(index_last)
-     
-        # Scale around center pivot - ts and tp are ineffectual for magnitude scaling
+
+        # Scale around center pivot.
         cmds.scaleKey(
-            curve_obj,
+            curve_obj[0],
             includeUpperBound=False,
-            timeScale=1,
-            timePivot=52,
             index=(key_insert_start, key_insert_end),
             valueScale=scale_amount,
             valuePivot=mid_point,
-            attribute=curve_attr
+            attribute=curve_attr[0]
         )
 
+    # ------------------------------------------------------------------- 
+    # 01. RESTORE LOCK STATE OF BASE ANIMATION LAYER.
+    # ------------------------------------------------------------------- 
     if cmds.objExists(base_anim_layer):
         if was_locked:
             cmds.animLayer(edit=True, lock=1, name=base_anim_layer)
