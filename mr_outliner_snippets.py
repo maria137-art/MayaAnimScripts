0008dd4a81c81e2bbdbdbac443f6c06ca01ace01
unknown <maria137.art@hotmail.co.uk>
2023-12-29 04:30:04 +0000

    Changing function names.

--- mr_outliner_snippets.py
+++ mr_outliner_snippets.py
@@ -1,7 +1,7 @@
 """
 # ------------------------------------------------------------------------------ #
 # SCRIPT: mr_outliner_snippets.py
-# VERSION: 0003
+# VERSION: 0004
 #
 # CREATORS: Maria Robertson
 # ---------------------------------------
@@ -10,39 +10,40 @@
 # DESCRIPTION: 
 # ---------------------------------------
 # A collection of scripts to work in the Outliner panel faster.
-# They all currently only work when the mouse cursor is above the Outliner.
+# They currently only work when the mouse cursor is above the Outliner.
 #
 # ---------------------------------------
 # RUN COMMANDS:
 # ---------------------------------------
 e.g.
-
 import importlib
 import mr_outliner_snippets
 importlib.reload(mr_outliner_snippets)
 
-mr_outliner_snippets.outliner_collapse_all_items_except_selected()
+mr_outliner_snippets.outliner_collapse_all_except_selected()
 
 # ---------------------------------------
 # CHANGELOG:
 # ---------------------------------------
-# 2023-07-09: 0003
+# 2023-12-29 - 0004:
+#   - Updating function names.
+#
+# 2023-07-09 - 0003:
 #   - Updating functions, so that the mouse cursor doesn't have to be
 #     above the Outliner, in order to trigger.
 #   - Adding outliner_expand_children_of_selected()
 #
-# 2023-07-09: 0002
+# 2023-07-09 - 0002:
 #   - Realised that Maya has default hotkeys for Outliner scripts in MEL.
 #   - Oh well, at least have Python versions now, in case.
 #
-# 2023-07-08: 0001 
+# 2023-07-08 - 0001 :
 #   - First pass.
 # ------------------------------------------------------------------------------ #
 """
 
 import maya.cmds as cmds
 import maya.mel as mel
-
 
 def get_visible_outliner_panels():
     outliner_panels = cmds.getPanel(type='outlinerPanel')
@@ -52,35 +53,30 @@ def get_visible_outliner_panels():
     return visible_outliner_panels
 
 # ------------------------------------------------------------------- 
-# 00. FOR ALL
+# 00. FOR ALL ITEMS.
 # ------------------------------------------------------------------- 
-
 def outliner_expand_all_items():
     visible_outliner_panels = get_visible_outliner_panels()
     [cmds.outlinerEditor(outliner, edit=True, expandAllItems=True) for outliner in visible_outliner_panels]     
 
 # ------------------------------------------------------------------- 
-
 def outliner_collapse_all_items():    
     visible_outliner_panels = get_visible_outliner_panels()
     [cmds.outlinerEditor(outliner, edit=True, expandAllItems=False) for outliner in visible_outliner_panels]     
 
 # ------------------------------------------------------------------- 
-# 00. FOR SELECTED
+# 00. FOR SELECTED ITEMS.
 # ------------------------------------------------------------------- 
-
-def outliner_expand_all_selected_items():
+def outliner_expand_selected_items():
     visible_outliner_panels = get_visible_outliner_panels()
     [cmds.outlinerEditor(outliner, edit=True, expandAllSelectedItems=True) for outliner in visible_outliner_panels]         
 
 # ------------------------------------------------------------------- 
-
-def outliner_collapse_all_selected_items():
+def outliner_collapse_selected_items():
     visible_outliner_panels = get_visible_outliner_panels()
     [cmds.outlinerEditor(outliner, edit=True, expandAllSelectedItems=False) for outliner in visible_outliner_panels]         
 
 # ------------------------------------------------------------------- 
-
 def outliner_expand_children_of_selected():
     # Get visible outliners
     visible_outliner_panels = get_visible_outliner_panels()
@@ -99,10 +95,9 @@ def outliner_expand_children_of_selected():
     [cmds.outlinerEditor(outliner, edit=True, showSelected=True) for outliner in visible_outliner_panels]      
 
 # ------------------------------------------------------------------- 
-# 00. FOR ALL EXCEPT SELECTED
+# 00. FOR ALL EXCEPT SELECTED ITEMS.
 # -------------------------------------------------------------------
-
-def outliner_collapse_all_items_except_selected():
+def outliner_collapse_all_except_selected():
     visible_outliner_panels = get_visible_outliner_panels()
     [cmds.outlinerEditor(outliner, edit=True, expandAllItems=False) for outliner in visible_outliner_panels]         
 
