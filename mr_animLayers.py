f92129ae920a988fc6ece3478b03739bfcf7926d
unknown <maria137.art@hotmail.co.uk>
2024-01-16 05:06:52 +0000

    Adding more functions.
    # 2024-01-16 - 0006:
    #   - Add following functions for mr_utilities.
    #       - reset_animation_layer_keys_at_currentTime()
    #       - nullify_animation_layer_keys()

--- mr_animLayers.py
+++ mr_animLayers.py
@@ -1,7 +1,7 @@
 """
 # ------------------------------------------------------------------------------ #
 # SCRIPT: mr_animLayers.py
-# VERSION: 0005
+# VERSION: 0006
 #
 # CREATORS: Maria Robertson
 # ---------------------------------------
@@ -9,8 +9,7 @@
 # ---------------------------------------
 # DESCRIPTION: 
 # ---------------------------------------
-# Create a new additive animation layer for selected objects that has the same
-# number of keyframes as the BaseAnimation layer.
+
 #
 # ---------------------------------------
 # RUN COMMAND:
@@ -21,37 +20,19 @@ importlib.reload(mr_animLayers)
 
 # EXAMPLES
 mr_animLayers.create_animation_layer_with_baseAnimation_keyTiming(override_layerMode=False)
-mr_animLayers.bake_to_selected_override_animation_layer(simulation=True)
+mr_animLayers.bake_to_selected_override_animation_layer(simulation=True, preserveOutsideKeys=True)
 
-# ---------------------------------------
-# CHANGELOG:
-# ---------------------------------------
-# 2024-01-14 - 0005:
-#   - Converting and merging mr_animLayer_createEmptyLayerWithKeyTimingFromSelected.mel to Python.
-#
-# 2024-01-03 - 0004:
-#   - mr_animLayer_bake_to_override()
-#       - Suspend viewport during bake.
-#       - Add option to simulate bake or not.
-#
-# 2024-01-03 - 0003:
-#   - mr_animLayer_bake_to_override()
-#       - Avoid major bug when "BaseAnimation" is selected, by not using -destinationLayer with it during bakeResults.
-#
-# 2023-12-18 - 0002:
-#   - mr_animLayer_bake_to_override()
-#       - Ensuring no keys are selected before baking, to prevent Maya error.
-#
-# 2023-11-19 - 0001:
-#   - mr_animLayer_bake_to_override()
-#       - 1st pass of script. Created after working on multiple cycles in one file, each on an Override Layer.
 # ------------------------------------------------------------------------------ #
 """
 
 import maya.cmds as cmds
 
+import importlib
+import mr_utilities
+importlib.reload(mr_utilities)
+
 # ------------------------------------------------------------------------------ #
-def bake_to_selected_override_animation_layer(simulation=True):
+def bake_to_selected_override_animation_layer(simulation=True, preserveOutsideKeys=True):
     """
     Working with multiple Override animation layers in a scene, and wanting to bake additve layers to it.
     Personally find it better than using Maya's Default Merge Layers, when dealing with a scene with multiple override animation layers.
@@ -96,13 +77,15 @@ def bake_to_selected_override_animation_layer(simulation=True):
                 # Otherwise, for some reason it stops user from setting keys on the object, until all animation layers are deleted.
                 cmds.bakeResults(
                     simulation=simulation,
-                    time=(min_time, max_time)
+                    time=(min_time, max_time),
+                    preserveOutsideKeys=preserveOutsideKeys
                 )
             else:
                 cmds.bakeResults(
                     destinationLayer=selected_animation_layers[0],
                     simulation=simulation,
-                    time=(min_time, max_time)
+                    time=(min_time, max_time),
+                    preserveOutsideKeys=preserveOutsideKeys
                 )
             cmds.refresh(suspend=0)
             # Clear any old warning messages.
@@ -113,6 +96,13 @@ def bake_to_selected_override_animation_layer(simulation=True):
 
 # ------------------------------------------------------------------------------ #
 def create_animation_layer_with_baseAnimation_keyTiming(override_layerMode=False):
+    """
+    Create a new additive animation layer for selected objects,
+    that has the same umber of keyframes as the BaseAnimation layer.
+
+    :param override_layerMode: If True, create an Override animation layer, instead of Additive.
+    :type override_layerMode: bool
+    """
     # ---------------------------------------
     # 01. CREATE ANIMATION LAYER.
     # ---------------------------------------
@@ -147,7 +137,7 @@ def create_animation_layer_with_baseAnimation_keyTiming(override_layerMode=False
     # 01. COPY BASEANIMATION KEY TIMING.
     # ---------------------------------------
     # Deselect all animation layers.
-    set_selected_for_all_layers(0)
+    mr_utilities.set_selected_for_all_layers(0)
     # Select just BaseAnimation.
     cmds.animLayer("BaseAnimation", edit=True, selected=True)
 
@@ -168,20 +158,184 @@ def create_animation_layer_with_baseAnimation_keyTiming(override_layerMode=False
     cmds.select(selection, replace=True)
 
     # Select the new animation layer.
-    set_selected_for_all_layers(0)
+    mr_utilities.set_selected_for_all_layers(0)
     cmds.animLayer(animation_layer, edit=True, selected=True)
 
 ##################################################################################################################################################
 
-########################################################################
-#                                                                      #
-#                           SUPPORT FUNCTIONS                          #
-#                                                                      #
-########################################################################
+# ------------------------------------------------------------------------------ #
+def reset_animation_layer_keys_at_currentTime(filter_selected_animation_layers=False, reset_non_numeric_attributes=False, reset_selected_attributes=False):
+    """
+    Reset keys of animation layers at the current time for selected objects.
+    This works the same as setting an animation layer's weight to 0, setting a key to store the current pose frame, then restoring its weight.
+
+    :param filter_selected_animation_layers: If True, reset only for the currently selected animation layers.
+    :type filter_selected_animation_layers: bool
+    :param reset_non_numeric_attributes: If True, reset non-numeric attributes.
+    :type reset_non_numeric_attributes: bool
+    :param reset_selected_attributes: If True, reset only the selected attributes on the objects.
+    :type reset_selected_attributes: bool
+
+    :Example:
+
+    >>>  (
+    ...     filter_selected_animation_layers=True,
+    ...     reset_non_numeric_attributes=False,
+    ...     reset_selected_attributes=True
+    ... )
+
+    """
+
+    # ---------------------------------------
+    # 01. GET SELECTION.
+    # ---------------------------------------
+    selection = mr_utilities.get_selection()
+
+    """
+    selection = mr_utilities.get_selection_generator()
+    if not any(selection):
+        sys.exit("Script terminated: No objects are selected.")
+    """
+    # ---------------------------------------
+    # 01. OPTIONAL - CHECK IF ANIMATION LAYERS ARE SELECTED.
+    # ---------------------------------------
+    if filter_selected_animation_layers:
+        tool_name = "AnimLayerTab"
+        selected_animation_layers = cmds.treeView(tool_name + "animLayerEditor", query=True, selectItem=True)
+        if not selected_animation_layers:
+            mr_utilities.print_warning_from_caller("No animation layers are highlighted.")
+            return
+
+    # ---------------------------------------
+    # 01. CHECK IF CONNECTED ANIMATION LAYERS ARE LOCKED OR MUTED.
+    # ---------------------------------------
+    connected_animation_layers = []
+
+    for obj in selection:
+        animation_layers = cmds.listConnections(obj, type="animLayer")
+        for layer in animation_layers:
+            connected_animation_layers.append(layer)
+
+    # Remove duplicates.
+    connected_animation_layers = list(set(connected_animation_layers))
+
+    if filter_selected_animation_layers:
+        connected_animation_layers = mr_utilities.filter_for_selected_animation_layers(connected_animation_layers)
+
+    if connected_animation_layers:
+        for layer in connected_animation_layers:
+            mute_state = cmds.getAttr(layer + ".mute")
+            lock_state = cmds.getAttr(layer + ".lock")
+
+            if mute_state and lock_state:
+                mr_utilities.print_warning_from_caller(f"\"{layer}\" is muted and locked.")
+                return
+            elif mute_state:
+                mr_utilities.print_warning_from_caller(f"\"{layer}\" is muted.")
+                return
+            elif lock_state:
+                mr_utilities.print_warning_from_caller(f"\"{layer}\" is locked.")
+                return
+    else:
+        mr_utilities.print_warning_from_caller("No connection found to queried animation layers.")
+        return
+
+    # ---------------------------------------
+    # 01. RESET KEYS.
+    # ---------------------------------------
+    nullify_animation_layer_keys(
+        selection=selection, 
+        attributes_to_reset=None, 
+        reset_selected_attributes=reset_selected_attributes, 
+        reset_non_numeric_attributes=reset_non_numeric_attributes, 
+        nullify_only_selected_animation_layers=filter_selected_animation_layers
+    )
 
 # ------------------------------------------------------------------------------ #
-def set_selected_for_all_layers(state):
-    # Python version of setSelectedForAllLayers from Autodesk Maya's layerEditor.mel, line 1220
-    layers = cmds.ls(type='animLayer')
-    for layer in layers:
-        cmds.animLayer(layer, edit=True, selected=state)
+def nullify_animation_layer_keys(selection=None, attributes_to_reset=None, reset_selected_attributes=False, reset_non_numeric_attributes=False, nullify_only_selected_animation_layers=False):
+    if not selection:
+        mr_utilities.print_warning_from_caller("Nothing is selected.")
+
+    for obj in selection:
+        # ---------------------------------------
+        # 02. IF NO ATTRIBUTES SPECIFIED, USE ALL KEYABLE.
+        # ---------------------------------------
+        if not attributes_to_reset:
+            if reset_selected_attributes:
+                attributes_to_reset = mr_utilities.get_selected_channels() or cmds.listAttr(obj, keyable=True)
+            else:
+                attributes_to_reset = cmds.listAttr(obj, keyable=True)
+
+        # ---------------------------------------
+        # 02. FILTER KEYS TO NULLIFY.
+        # ---------------------------------------
+        if attributes_to_reset:
+
+            # ---------------------------------------
+            # 03. OPTIONAL - FILTER FOR ATTRIBUTES ON SELECTED ANIMATON LAYERS.
+            # ---------------------------------------    
+            if nullify_only_selected_animation_layers:
+                layered_attributes = mr_utilities.get_layered_attributes(obj, filter_selected_animation_layers=True)
+            else:
+                layered_attributes = mr_utilities.get_layered_attributes(obj, filter_selected_animation_layers=False)
+
+            for layer, attributes in layered_attributes.items():
+                # ---------------------------------------
+                # 03. OPTIONAL - FILTER FOR SELECTED ATTRIBUTES.
+                # ---------------------------------------
+                if reset_selected_attributes:
+                    for attr in attributes:
+                        if attr not in attributes_to_reset:
+                            attributes.remove(attr)
+                # ---------------------------------------
+                # 03. OPTIONAL - FILTER OUT NON-NUMERIC ATTRIBUTES
+                # ---------------------------------------
+                if not reset_non_numeric_attributes:
+                    for attr in attributes:
+                        if not mr_utilities.is_attribute_numeric(obj, attr):
+                            attributes.remove(attr)
+                # ---------------------------------------
+                # 03. SET KEYS.
+                # ---------------------------------------
+                cmds.setKeyframe(obj, animLayer=layer, attribute=attributes, identity=True)
+        else:
+            continue
+
+    # Set to current time again, to force the viewport to update with the change.
+    current_time = cmds.currentTime(query=True)
+    cmds.currentTime(current_time, edit=True)
+
+
+##################################################################################################################################################
+"""
+# ---------------------------------------
+# CHANGELOG:
+# ---------------------------------------
+# 2024-01-16 - 0006:
+#   - Add following functions for mr_utilities.
+#       - reset_animation_layer_keys_at_currentTime()
+#       - nullify_animation_layer_keys()
+#
+# 2024-01-14 - 0005:
+#   - Converting and merging mr_animLayer_createEmptyLayerWithKeyTimingFromSelected.mel to Python.
+#   - bake_to_selected_override_animation_layer():
+#       - Adding option to preserve outside keys.
+#
+# 2024-01-03 - 0004:
+#   - mr_animLayer_bake_to_override()
+#       - Suspend viewport during bake.
+#       - Add option to simulate bake or not.
+#
+# 2024-01-03 - 0003:
+#   - mr_animLayer_bake_to_override()
+#       - Avoid major bug when "BaseAnimation" is selected, by not using -destinationLayer with it during bakeResults.
+#
+# 2023-12-18 - 0002:
+#   - mr_animLayer_bake_to_override()
+#       - Ensuring no keys are selected before baking, to prevent Maya error.
+#
+# 2023-11-19 - 0001:
+#   - mr_animLayer_bake_to_override()
+#       - 1st pass of script. Created after working on multiple cycles in one file, each on an Override Layer.
+# ------------------------------------------------------------------------------ #
+"""
